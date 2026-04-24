5069725e4da146f724c3d14d0d08135674dec6c6
Alice <langlois.alice@protonmail.com>
2026-04-24 11:24:39 +0200

    paginate videos page component

---
+++ src/components/PaginateVideos/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useEffect, useMemo } from "react";
+import { Pagination, usePagination } from "@openfun/cunningham-react";
+import { Video } from "@/src/types/interface";
+import VideosList from "@/src/components/VideosList/page";
+import styles from "./page.module.css";
+
+interface PaginateVideosProps {
+  videos: Video[];
+  pageSize?: number;
+  currentUserId?: number;
+}
+
+export default function PaginateVideos({
+  videos,
+  pageSize = 20,
+  currentUserId,
+}: PaginateVideosProps) {
+  const pagination = usePagination({
+    defaultPagesCount: 1,
+    defaultPage: 1,
+    pageSize,
+  });
+
+  const { page, setPage, pagesCount, setPagesCount } = pagination;
+
+  useEffect(() => {
+    const nextPagesCount = Math.max(1, Math.ceil(videos.length / pageSize));
+    setPagesCount(nextPagesCount);
+    setPage((currentPage) => Math.min(currentPage, nextPagesCount));
+  }, [videos.length, pageSize, setPage, setPagesCount]);
+
+  const paginatedVideos = useMemo(() => {
+    const start = (page - 1) * pageSize;
+    return videos.slice(start, start + pageSize);
+  }, [videos, page, pageSize]);
+
+  return (
+    <>
+      <VideosList videosList={paginatedVideos} currentUserId={currentUserId} />
+      {pagesCount && pagesCount > 1 && (
+        <div className={styles.pagination}>
+          <Pagination {...pagination} pageSize={pageSize} displayGoto={false} />
+        </div>
+      )}
+    </>
+  );
+}
