a666fdf13906290b85b32283b0a895db5b1c4ee6
Alice <langlois.alice@protonmail.com>
2026-04-24 11:13:48 +0200

    concat video card title

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -29,6 +29,11 @@ export function capitalize(value: string) {
   return value.length ? value[0].toUpperCase() + value.slice(1) : value;
 }
 
+export function truncateVideoTitle(value: string, maxLength = 18) {
+  if (value.length > maxLength) return value.slice(0, maxLength) + "...";
+  else return value;
+}
+
 export function setInitial(lastname: string, firstname: string) {
   return lastname
     .concat(" ", firstname)
